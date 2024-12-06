a6376242822995bc28519f30478d017b6ee7ceec
adrian7123 <bueno1dad@gmail.com>
2024-12-06 10:24:44 -0300

    feat(i18n): add brazilian portuguese language support

--- apps/dokploy/next-i18next.config.cjs
+++ apps/dokploy/next-i18next.config.cjs
@@ -1,10 +1,21 @@
 /** @type {import('next-i18next').UserConfig} */
 module.exports = {
-	i18n: {
-		defaultLocale: "en",
-		locales: ["en", "pl", "ru", "fr", "de", "tr", "zh-Hant", "zh-Hans", "fa"],
-		localeDetection: false,
-	},
-	fallbackLng: "en",
-	keySeparator: false,
+  i18n: {
+    defaultLocale: "en",
+    locales: [
+      "en",
+      "pl",
+      "ru",
+      "fr",
+      "de",
+      "tr",
+      "zh-Hant",
+      "zh-Hans",
+      "fa",
+      "pt-br",
+    ],
+    localeDetection: false,
+  },
+  fallbackLng: "en",
+  keySeparator: false,
 };
--- apps/dokploy/utils/hooks/use-locale.ts
+++ apps/dokploy/utils/hooks/use-locale.ts
@@ -1,29 +1,30 @@
 import Cookies from "js-cookie";
 
 const SUPPORTED_LOCALES = [
-	"en",
-	"pl",
-	"ru",
-	"fr",
-	"de",
-	"tr",
-	"zh-Hant",
-	"zh-Hans",
-	"fa",
+  "en",
+  "pl",
+  "ru",
+  "fr",
+  "de",
+  "tr",
+  "zh-Hant",
+  "zh-Hans",
+  "fa",
+  "pt-br",
 ] as const;
 
 type Locale = (typeof SUPPORTED_LOCALES)[number];
 
 export default function useLocale() {
-	const currentLocale = (Cookies.get("DOKPLOY_LOCALE") ?? "en") as Locale;
+  const currentLocale = (Cookies.get("DOKPLOY_LOCALE") ?? "en") as Locale;
 
-	const setLocale = (locale: Locale) => {
-		Cookies.set("DOKPLOY_LOCALE", locale, { expires: 365 });
-		window.location.reload();
-	};
+  const setLocale = (locale: Locale) => {
+    Cookies.set("DOKPLOY_LOCALE", locale, { expires: 365 });
+    window.location.reload();
+  };
 
-	return {
-		locale: currentLocale,
-		setLocale,
-	};
+  return {
+    locale: currentLocale,
+    setLocale,
+  };
 }
